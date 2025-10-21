2e38c1eee493ee1307ac398cdcbf40d1e4cef5cc
Quanbro07 <ngocquan612006@gmail.com>
2025-10-21 07:53:18 +0700

    Set up

---
+++ Backend/Loko-Backend/src/test/java/com/exproject/lokobackend/LokoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exproject.lokobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
