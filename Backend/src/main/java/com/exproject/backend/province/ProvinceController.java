e151915d11fffbd654e11415c9c1e0812701ee1c
Quanbro07 <ngocquan612006@gmail.com>
2025-11-27 15:39:20 +0700

    test

--- Backend/src/main/java/com/exproject/backend/province/ProvinceController.java
+++ Backend/src/main/java/com/exproject/backend/province/ProvinceController.java
@@ -1,8 +1,10 @@
 package com.exproject.backend.province;
 
 import com.exproject.backend.location.dto.LocationResponse;
+import com.exproject.backend.province.dto.ProvinceDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +16,15 @@ public class ProvinceController {
 
     private final ProvinceService provinceService;
 
+    @PreAuthorize("authentication.principal.id == #userId")
+    @GetMapping("/getAll")
+    public ResponseEntity<List<ProvinceDTO>> getVisitedProvinces(
+            @RequestParam Long userId) {
+
+        List<ProvinceDTO> response = provinceService.getAllProvince(userId);
+
+        return ResponseEntity.ok(response);
+    }
 
 
 }
--- Backend/src/main/java/com/exproject/backend/province/ProvinceService.java
+++ Backend/src/main/java/com/exproject/backend/province/ProvinceService.java
@@ -2,9 +2,12 @@ package com.exproject.backend.province;
 
 import com.exproject.backend.location.LocationRepository;
 import com.exproject.backend.location.dto.LocationResponse;
+import com.exproject.backend.province.dto.ProvinceDTO;
+import com.exproject.backend.province.info.Province;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,5 +17,20 @@ public class ProvinceService {
     private final ProvinceRepository provinceRepository;
 
 
+    public List<ProvinceDTO> getAllProvince(Long userId) {
+        List<Province> visiedProvinces = provinceRepository.findAllByVisitedUsers_Id(userId);
 
+        List<ProvinceDTO> provinceDTOs = visiedProvinces.stream()
+                .map(this::convertToDTO)
+                .toList();
+
+        return provinceDTOs;
+    }
+
+    private ProvinceDTO convertToDTO(Province province) {
+        return ProvinceDTO.builder()
+                .provinceId(province.getId())
+                .provinceName(province.getProvinceName())
+                .build();
+    }
 }
