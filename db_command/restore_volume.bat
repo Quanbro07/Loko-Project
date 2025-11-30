a9f1466b3dd7411409d3b354298525e7ed68c107
chovy02 <luonggiabao0202@gmail.com>
2025-11-30 15:10:42 +0700

    fix

--- db_command/restore_volume.bat
+++ db_command/restore_volume.bat
@@ -1,5 +1,5 @@
 @echo off
-set VOLUME_NAME=loko-project_db
+set VOLUME_NAME=loko-project_db_data
 set BACKUP_FILE=db_data_backup.tar.gz
 
 echo Đang restore dữ liệu vào volume "%VOLUME_NAME%"...
@@ -10,7 +10,7 @@ echo Dung container dang su dung volume (neu co)...
 docker stop postgres_container_name 2>NUL
 
 docker run --rm ^
-    -v %VOLUME_NAME/:to% ^
+    -v %VOLUME_NAME%:/to ^
     -v "%cd%":/from ^
     alpine sh -c "cd /to && tar -xzvf /from/%BACKUP_FILE%"
 
